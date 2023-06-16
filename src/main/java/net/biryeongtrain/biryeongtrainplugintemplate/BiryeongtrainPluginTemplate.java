af4f78068f5786a45324635e50da94bc742a8ae6
biryeongtrain <biryeongtrain06@gmail.com>
2023-06-17 01:18:39 +0900

    init

---
+++ src/main/java/net/biryeongtrain/biryeongtrainplugintemplate/BiryeongtrainPluginTemplate.java
@@ -0,0 +1,17 @@
+package net.biryeongtrain.biryeongtrainplugintemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BiryeongtrainPluginTemplate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
